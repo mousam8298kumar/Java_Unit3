fa48f095b922c5fd90eeef416fa1605274b47f5f
mousam8298kumar <mousam8298kumar@gmail.com>
2026-02-07 10:11:46 +0530

    Add FinallyDemo class to showcase exception handling
    Demonstrate the use of try-catch-finally with exception handling.

---
+++ java_files/FinallyDemo.java
@@ -0,0 +1,13 @@
+public class FinallyDemo {
+public static void main(String[] args) {
+try {
+int a = 10 / 0; // Causes exception
+}
+catch (ArithmeticException e) {
+System.out.println("Error: " + e.getMessage());
+}
+finally {
+System.out.println("This will always run, no matter what.");
+}
+}
+}
