e99c35609a11d42a5c0267099c78f7bc8e3551b9
mousam8298kumar <mousam8298kumar@gmail.com>
2026-02-07 09:31:43 +0530

    Create FileReadExample.java for file reading demo
    Add a basic file reading example that attempts to open a non-existent file.

---
+++ java_files/FileReadExample.java
@@ -0,0 +1,6 @@
+import java.io.*;
+public class FileReadExample {
+public static void main(String[] args) throws FileNotFoundException {
+FileInputStream fis = new FileInputStream("no-such-file.txt");
+}
+}
